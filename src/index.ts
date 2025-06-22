2d82297eb3f118f6d66a3632ff11acb10c665b54
Rupam Das <rejisterjack@gmail.com>
2025-06-22 11:48:49 +0530

    feat: swagger

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,23 @@
-import { Elysia } from "elysia";
+import { Elysia } from 'elysia'
+import { cors } from '@elysiajs/cors'
+import { swagger } from '@elysiajs/swagger'
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .use(cors())
+  .use(
+    swagger({
+      path: '/docs',
+      documentation: {
+        info: {
+          title: 'BookBazaar API Documentation',
+          version: '1.0.0',
+        },
+      },
+    })
+  )
+  .get('/', () => 'Hello Elysia')
+  .listen(3000)
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+)
